4da9b65c4f46e8ddedc5b3738cc9166c791605de
Bori7 <oluwaboriife@gmail.com>
2022-03-17 20:30:30 +0100

    deploying ERC721

--- contracts/BoriERC721.sol
+++ contracts/BoriERC721.sol
@@ -1,16 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
-import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
-contract newNFT is NFTokenMetadata, Ownable {
 
-  constructor() {
-    nftName = "BoriNFT";
-    nftSymbol = "BNFT";
-  }
-  function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
-    super._mint(_to, _tokenId);
-    super._setTokenUri(_tokenId, _uri);
-  }
- 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract BoriNFT is ERC721, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("BoriNFT", "BoNFT") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://ipfs.io/ipfs/";
+    }
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 }
